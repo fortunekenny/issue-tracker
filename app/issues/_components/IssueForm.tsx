43bb6e83f3aa2ad6ec1fbb463cb3eebc305e55d5
fortunekenny <gbephraim@gmail.com>
2024-06-20 07:53:48 +0100

    updating issues

--- app/issues/_components/IssueForm.tsx
+++ app/issues/_components/IssueForm.tsx
@@ -8,7 +8,7 @@ import axios from "axios";
 import { useRouter } from "next/navigation";
 import { useState } from "react";
 import { zodResolver } from "@hookform/resolvers/zod";
-import { createIssueSchema } from "@/app/validationSchemas";
+import { issueSchema } from "@/app/validationSchemas";
 import { z } from "zod";
 import ErrorMessage from "@/app/components/ErrorMessage";
 import Spinner from "@/app/components/Spinner";
@@ -17,7 +17,7 @@ import { Issue } from "@prisma/client";
 const SimpleMDE = dynamic(() => import('react-simplemde-editor'), { ssr: false });
 
 
-type IssueFormData = z.infer<typeof createIssueSchema>
+type IssueFormData = z.infer<typeof issueSchema>
 
 
 
@@ -25,7 +25,7 @@ const IssueForm = ({issue}:{issue?:Issue}) => {
 
     const router = useRouter()
     const {register, control, handleSubmit, formState: {errors} } = useForm<IssueFormData>({
-        resolver: zodResolver(createIssueSchema)
+        resolver: zodResolver(issueSchema)
     })
     const [error, setError] = useState('')
     const [isSubmitting, setSubmitting] = useState(false)
