ae98bcb6b49b8bfc158e1c855d486a41c76d6e0b
fortunekenny <gbephraim@gmail.com>
2024-06-21 12:47:43 +0100

    deleting issues

--- app/api/issues/[id]/route.ts
+++ app/api/issues/[id]/route.ts
@@ -1,5 +1,6 @@
 import { issueSchema } from "@/app/validationSchemas";
 import prisma from "@/prisma/client";
+import { Prisma } from "@prisma/client";
 import { NextRequest, NextResponse } from "next/server";
 
 export async function PATCH(request: NextRequest, {params}:{params:{id:string}}){
@@ -24,5 +25,23 @@ export async function PATCH(request: NextRequest, {params}:{params:{id:string}})
     })
 
     return NextResponse.json(updatedIssue)
+
+}
+
+
+
+
+export async function DELETE(request: NextRequest, {params}:{params:{id:string}}){
+    const issue = await prisma.issue.findUnique({
+        where: {id: parseInt(params.id)}
+    })
+
+    if (!issue)
+        return NextResponse.json({error: 'Invalid issue'}, {status: 404})
+
+    await prisma.issue.delete({
+        where: {id: issue.id}
+    })
+        return NextResponse.json({})
 
 }
