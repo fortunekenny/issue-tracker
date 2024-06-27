54590cf6240d20f0691cd5f1a3ea0367f53fc4e4
fortunekenny <gbephraim@gmail.com>
2024-06-27 17:48:29 +0100

    assigning issues to users

---
+++ app/issues/[id]/AssigneeSelect.tsx
@@ -0,0 +1,20 @@
+'use client'
+
+
+
+import { Select } from "@radix-ui/themes"
+
+const AssigneeSelect = () => {
+  return (
+    <Select.Root>
+        <Select.Trigger placeholder='Assign...'/>
+        <Select.Content>
+            <Select.Group>
+                <Select.Label>Suggestion</Select.Label>
+                <Select.Item value='1'>Ken Forte</Select.Item>
+            </Select.Group>
+        </Select.Content>
+    </Select.Root>
+  )
+}
+export default AssigneeSelect
