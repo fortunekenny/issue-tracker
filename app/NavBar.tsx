5a7989a7fd5746c8b4876d34dfa6db4e9ac2fc50
fortunekenny <gbephraim@gmail.com>
2024-06-27 16:31:13 +0100

    authentication

--- app/NavBar.tsx
+++ app/NavBar.tsx
@@ -1,12 +1,13 @@
 'use client'
 
 
-import { Avatar, Box, Button, Container, DropdownMenu, Flex, Text } from "@radix-ui/themes"
+import { Avatar, Box, Container, DropdownMenu, Flex, Text } from "@radix-ui/themes"
 import classNames from "classnames"
 import { useSession } from "next-auth/react"
 import Link from "next/link"
 import { usePathname } from "next/navigation"
 import {AiFillBug} from 'react-icons/ai'
+import { Skeleton } from "@/app/components"
 
 
 const NavBar = () => {
@@ -62,14 +63,13 @@ const AuthStatus = () => {
 
     const {status, data: session} = useSession()
 
-    if (status === 'loading') return null
+    if (status === 'loading') return  <Skeleton width='3rem'/>
 
     if (status === 'unauthenticated')
         return <Link className="nav-link" href='/api/auth/signin'>Login</Link>
 
     return  (               
         <Box>
-            
                 <DropdownMenu.Root>
                     <DropdownMenu.Trigger>
                         <Avatar 
