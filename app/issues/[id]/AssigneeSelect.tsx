0608537af2c5cc1e974067f06d8b0395b4bd3e1a
fortunekenny <gbephraim@gmail.com>
2024-06-30 13:01:10 +0100

    assign issue to user

--- app/issues/[id]/AssigneeSelect.tsx
+++ app/issues/[id]/AssigneeSelect.tsx
@@ -6,6 +6,7 @@ import { useQuery } from "@tanstack/react-query"
 import axios from "axios"
 import {Skeleton } from '@/app/components'
 import { Issue } from "@prisma/client"
+import toast, {Toaster} from 'react-hot-toast'
 
 
 
@@ -33,19 +34,24 @@ if (isLoading) return <Skeleton/>
 if (error) return null;
 
   return (
-    <Select.Root defaultValue={issue.assignedToUserId || ''} onValueChange={(userId) => {
-      axios.patch(`/api/issues/${issue.id}`, {assignedToUserId: userId || null})
-    }}>
-        {/* <Select.Trigger placeholder={"Pick a fruit"} /> */}
-        <Select.Trigger />
-        <Select.Content>
-            <Select.Group>
-              <Select.Label>Suggestion</Select.Label>
-              <Select.Item value=''>Unassigned</Select.Item>
-                {users?.map(user => <Select.Item key={user.id} value={user.id}>{user.name}</Select.Item>)}
-            </Select.Group>
-        </Select.Content>
-    </Select.Root>
+    <>
+      <Select.Root defaultValue={issue.assignedToUserId || ''} onValueChange={(userId) => {
+        axios
+          .patch(`/api/issues/${issue.id}`, {assignedToUserId: userId || null})
+          .catch(() => {toast.error('Changes could not be saved.')})
+      }}>
+          {/* <Select.Trigger placeholder={"Pick a fruit"} /> */}
+          <Select.Trigger />
+          <Select.Content>
+              <Select.Group>
+                <Select.Label>Suggestion</Select.Label>
+                <Select.Item value=''>Unassigned</Select.Item>
+                  {users?.map(user => <Select.Item key={user.id} value={user.id}>{user.name}</Select.Item>)}
+              </Select.Group>
+          </Select.Content>
+      </Select.Root>
+      <Toaster/>
+    </>
   )
 }
 export default AssigneeSelect
